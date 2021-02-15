8d96e0625cec09f025d77ccfe9a6570557817410
ljsunny <43569683+ljsunny@users.noreply.github.com>
2021-02-15 15:30:51 +0900

    javascript: navbar

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Hello World");
+// navbar를 투명하게
+
+var nav = document.querySelector("#navbar");
+var nav_height = nav.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  console.log(nav_height);
+  if (window.scrollY > nav_height) {
+    nav.classList.add("navbar--dark");
+  } else {
+    nav.classList.remove("navbar--dark");
+  }
+});
