ad0f6033a0f0355a69e17fe55a14467f690e6474
ljsunny <43569683+ljsunny@users.noreply.github.com>
2021-03-15 17:00:06 +0900

    Nav underbar move when scrolling

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 "use strict";
 
-// navbar를 투명하게
+/*/////////////////////////////////////////////////////////////
+Navbar transparent when home screen
+///////////////////////////////////////////////////////////////*/
 const nav = document.querySelector("#navbar");
 const navHeight = nav.getBoundingClientRect().height;
 const navText = nav.querySelector(".nav__text");
@@ -16,7 +18,9 @@ document.addEventListener("scroll", () => {
   }
 });
 
-//section으로 이동
+/*/////////////////////////////////////////////////////////////
+Go to Section
+///////////////////////////////////////////////////////////////*/
 const navbarMenu = document.querySelector(".navbar__menu");
 
 navbarMenu.addEventListener("click", (event) => {
@@ -34,7 +38,9 @@ navbarMenu.addEventListener("click", (event) => {
   scrollIntoView(link);
 });
 
-//toggle 클릭시 버튼 활성화
+/*/////////////////////////////////////////////////////////////
+NavbarMenu Open when click toggle button
+///////////////////////////////////////////////////////////////*/
 const navbarToggle = document.querySelector(".navbar__toggle-btn");
 console.log(navbarToggle);
 navbarToggle.addEventListener("click", () => {
@@ -42,7 +48,9 @@ navbarToggle.addEventListener("click", () => {
   navbarMenu.classList.toggle("open");
 });
 
-// home의 contact me click 시 contact section으로 이동
+/*/////////////////////////////////////////////////////////////
+Go to Contact Section when click 'Contact Me' 
+///////////////////////////////////////////////////////////////*/
 const contactBtn = document.querySelector(".home__contact");
 console.log(contactBtn);
 contactBtn.addEventListener("click", () => {
@@ -51,7 +59,9 @@ contactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-// scrolling 시 home 투명하게
+/*/////////////////////////////////////////////////////////////
+Home transparent when Scrolling
+///////////////////////////////////////////////////////////////*/
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 
@@ -60,13 +70,18 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+/*/////////////////////////////////////////////////////////////
+Blur to Clear when page loaded
+///////////////////////////////////////////////////////////////*/
 const fullHome = document.querySelector("#home");
-//로드될때 블러에서 선명하게
 window.addEventListener("load", () => {
   console.log("page is fully loaded");
   fullHome.classList.add("clear");
 });
-// 스크롤 시 arrow up 버튼 visible
+
+/*/////////////////////////////////////////////////////////////
+Arrow-Up Button Visible when Scrolling
+///////////////////////////////////////////////////////////////*/
 const upBtn = document.querySelector(".up__toggle-btn");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -75,13 +90,16 @@ document.addEventListener("scroll", () => {
     upBtn.classList.remove("visible");
   }
 });
-
-// arrow up 버튼 클릭 시 go to home
+/*/////////////////////////////////////////////////////////////
+Go to Home when Arrow-Up button Click
+///////////////////////////////////////////////////////////////*/
 upBtn.addEventListener("click", () => {
   scrollIntoView("#home");
 });
 
-//works 카테고리 클릭시 filtering
+/*/////////////////////////////////////////////////////////////
+Filtering works when 'works category button' click
+///////////////////////////////////////////////////////////////*/
 const workCategories = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
@@ -97,7 +115,7 @@ workCategories.addEventListener("click", (event) => {
   const active = document.querySelector(".category__btn.active");
   active.classList.remove("active");
   setTimeout(() => {
-    //주의) 코드가 한번에 읽어서 실행되는 것을 방지하기 위해 setTimeout 안에 후속 작업 코딩
+    // notice) 코드가 한번에 읽어서 실행되는 것을 방지하기 위해 setTimeout 안에 후속 작업 코딩
     target.classList.add("active");
     projects.forEach((project) => {
       //console.log(project.dataset.category); arry형 foreach로
@@ -110,6 +128,34 @@ workCategories.addEventListener("click", (event) => {
 
     projectContainer.classList.remove("fade-out");
   }, 300);
+});
+/*/////////////////////////////////////////////////////////////
+Nav underbar move when scrolling (clientRect or IntersectionObserver need modify)
+///////////////////////////////////////////////////////////////*/
+window.addEventListener("scroll", () => {
+  const scrollh = window.scrollY;
+  const active = document.querySelector(".navbar__menu__item.active");
+  const navElement = navbarMenu.getElementsByTagName("li");
+  console.log(scrollh);
+  if (scrollh < 500) {
+    active.classList.remove("active");
+    navElement[0].classList.add("active");
+  } else if (scrollh < 1400) {
+    active.classList.remove("active");
+    navElement[1].classList.add("active");
+  } else if (scrollh < 2200) {
+    active.classList.remove("active");
+    navElement[2].classList.add("active");
+  } else if (scrollh < 3800) {
+    active.classList.remove("active");
+    navElement[3].classList.add("active");
+  } else if (scrollh < 4400) {
+    active.classList.remove("active");
+    navElement[4].classList.add("active");
+  } else {
+    active.classList.remove("active");
+    navElement[5].classList.add("active");
+  }
 });
 
 function scrollIntoView(selector) {
